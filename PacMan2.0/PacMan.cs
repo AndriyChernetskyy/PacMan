92a2032a974eadbc2efbd08ad79c3d3f91e828f6
Andriy <andrewusa921@gmail.com>
2018-06-19 00:41:56 +0300

    Rewriting
    Rewrite code, now it is correct and buildable

---
+++ PacMan2.0/PacMan.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacMan2._0
+{
+    public class PacMan : GameEngine, IEatable
+    {
+        public readonly string Symbol = "P";
+
+        //public new Position Position { get; set; }
+        //public new ConsoleColor Color { get; set; }
+
+
+        public PacMan(ConsoleColor color, Position position)
+        {
+            Color = ConsoleColor.Yellow;
+            Position = position;
+        }
+
+        public void Eat(Food food)
+        {
+            if (food == Food.PointFood)
+            {
+                this.Score++;
+            }
+
+            if (food == Food.Cherry)
+            {
+                this.Score += 100;
+            }
+
+            if (food == Food.Energizer)
+            {
+                this.Score += 50;
+            }
+
+            if (food == Food.Ghost)
+            {
+                this.Score += 200;
+            }
+        }
+
+
+    }
+}
